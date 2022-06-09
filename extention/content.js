01806641631be780c27f1e079461037548a9c654
priyanshu sharma <105141523+priyanshu-sharma-1@users.noreply.github.com>
2022-06-09 06:14:34 -0400

    Add files via upload

---
+++ extention/content.js
@@ -0,0 +1,9 @@
+if(window.location.hostname.includes('xx')){
+  document.body.innerHTML = "<h1>this site has been blocked by Whack-the-horny</h1>"
+}
+if(window.location.hostname.includes('porn')){
+    document.body.innerHTML = "<h1>this site has been blocked by Whack-the-horny</h1>"
+  }
+
+  /*priyanshu - so i had to write tw if statments cause idk the syntax 
+  to target two keywords in hostname in single if statment*/
